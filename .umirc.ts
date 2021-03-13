deedb7c82ff9a25a222f103f456590077e7a4939
xuzhiwei <gcdxuzhiwei@gmail.com>
2021-03-13 21:02:08 +0800

    icon

--- .umirc.ts
+++ .umirc.ts
@@ -18,6 +18,8 @@ export default defineConfig({
   devServer: {
     port: 8001,
   },
+  favicon:
+    'https://raw.githubusercontent.com/github/explore/80688e429a7d4ef2fca1e82350fe8e3517d3494d/topics/react/react.png',
   hash: true,
   routes: [
     {
